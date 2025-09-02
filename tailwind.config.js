67f2acff549392f2e15d2e990edb337d03dd4460
Léo JEGO <leo.jego56@gmail.com>
2025-09-02 14:28:48 +0200

    feat: init next app (App Router) + tailwind + layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}"
+    "./app/**/*.{ts,tsx,js,jsx}",
+    "./components/**/*.{ts,tsx,js,jsx}"
   ],
   theme: {
     extend: {},
